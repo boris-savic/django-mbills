cee1cd808a866b41da9b94e62c816b3eab678eef
Boris Savic <boris.savic@xlab.si>
2017-10-11 10:46:03 +0200

    - Updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mbills',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Django implementation of the Hal MBills APIs.',
-    long_description=README,
     url='https://github.com/boris-savic/django-mbills',
     author='Boris Savic',
     author_email='boris70@gmail.com',

a63cdf6306416c52f12f215cbd67115292648127
Boris Savic <boris.savic@xlab.si>
2017-10-11 10:51:21 +0200

    - Updated setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
-import os
-from setuptools import find_packages, setup
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
+from distutils.core import setup
 setup(
-    name='django-mbills',
-    version='0.1.1',
-    packages=find_packages(),
-    include_package_data=True,
-    license='MIT License',
-    description='Django implementation of the Hal MBills APIs.',
-    url='https://github.com/boris-savic/django-mbills',
-    author='Boris Savic',
-    author_email='boris70@gmail.com',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+  name='django-mbills',
+  packages=['django-mbills'],
+  version='0.1.3',
+  description='Django implementation of the Hal MBills APIs.',
+  author='Boris Savic',
+  author_email='boris70@gmail.com',
+  url='https://github.com/boris-savic/django-mbills',
+  keywords=['mbills', 'halcom', 'django' ],
+  classifiers=[],
+  install_requires=[
+        'python_mbills>=0.1.3',
+    ]
 )
